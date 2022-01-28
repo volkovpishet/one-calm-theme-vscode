9c230da835c0fb33cf8a150e44816cba447ac5fd
Andrey Volkov <vlkv.andrey@gmail.com>
2022-01-29 01:27:08 +0400

    revert some colours to original state

--- generator/theme/colors.ts
+++ generator/theme/colors.ts
@@ -3,9 +3,9 @@ import { Colors } from "../models"
 export const standardColors: Colors = {
   chalky: "#d3b297",
   coral: "#cf7575",
-  cyan: "#88cfc6",
+  cyan: "#56B6C2",
   error: "#f44747",
-  ivory: "#b6becc",
+  ivory: "#aeb7c8",
   malibu: "#5ab5ff",
   sage: "#98c379",
   stone: "#5c6370",
