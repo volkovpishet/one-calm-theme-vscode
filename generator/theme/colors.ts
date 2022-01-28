c356ce2f2addfd158e5248602ac42b95108e374a
Andrey Volkov <vlkv.andrey@gmail.com>
2022-01-28 15:07:02 +0400

    tweak colors

--- generator/theme/colors.ts
+++ generator/theme/colors.ts
@@ -2,13 +2,13 @@ import { Colors } from "../models"
 
 export const standardColors: Colors = {
   chalky: "#d4b497",
-  coral: "#eb7682",
-  cyan: "#00dbd0",
+  coral: "#f16b6b",
+  cyan: "#11b67f",
   error: "#f44747",
   ivory: "#abb2bf",
   malibu: "#6fbeff",
   sage: "#98c379",
   stone: "#5c6370",
   violet: "#c490ff",
-  whiskey: "#ebaa6d",
+  whiskey: "#e49d4d",
 }
